460b33653eb0ffa8fde7080a5606d60a55222dfa
Sam Parsons <samuel.m.parsons@gmail.com>
2018-10-27 20:41:19 -0400

    Implemented timer opcodes

---
+++ src/chip8/store/opcodes/timer.test.ts
@@ -0,0 +1,71 @@
+import { chip8InitialState } from 'src/chip8/store'
+
+import { setDelayTimerToRegister, setRegisterToDelayTimer, setSoundTimerToRegister } from './timer'
+
+describe('timer', () => {
+  describe('setRegisterToDelayTimer', () => {
+    const currentState = {
+      ...chip8InitialState,
+      opcode: 0xf207,
+      programCounter: 0x27d,
+      delayTimer: 0x13,
+      vRegisters: Object.assign([], chip8InitialState.vRegisters, {
+        2: 0x4a
+      })
+    }
+
+    const { vRegisters, programCounter } = setRegisterToDelayTimer(currentState)
+
+    it('sets delay timer to value stored in VX', () => {
+      expect(vRegisters[2]).toBe(0x13)
+    })
+
+    it('increments program counter by two', () => {
+      expect(programCounter).toBe(0x27f)
+    })
+  })
+
+  describe('setDelayTimerToRegister', () => {
+    const currentState = {
+      ...chip8InitialState,
+      opcode: 0xf515,
+      programCounter: 0x232,
+      delayTimer: 0x4b,
+      vRegisters: Object.assign([], chip8InitialState.vRegisters, {
+        5: 0x51
+      })
+    }
+
+    const { delayTimer, programCounter } = setDelayTimerToRegister(currentState)
+
+    it('sets delay timer to value stored in VX', () => {
+      expect(delayTimer).toBe(0x51)
+    })
+
+    it('increments program counter by two', () => {
+      expect(programCounter).toBe(0x234)
+    })
+  })
+
+  describe('setSoundTimerToRegister', () => {
+    const currentState = {
+      ...chip8InitialState,
+      opcode: 0xfa18,
+      programCounter: 0x280,
+      soundTimer: 0x2c,
+      vRegisters: Object.assign([], chip8InitialState.vRegisters, {
+        10: 0x3c
+      })
+    }
+
+    const { soundTimer, programCounter } = setSoundTimerToRegister(currentState)
+
+    it('sets delay timer to value stored in VX', () => {
+      expect(soundTimer).toBe(0x3c)
+    })
+
+    it('increments program counter by two', () => {
+      expect(programCounter).toBe(0x282)
+    })
+  })
+})
---
+++ src/chip8/store/opcodes/timer.ts
@@ -0,0 +1,53 @@
+import { Chip8, chip8Selectors, OpcodeFunc } from 'src/chip8/store'
+import { pipe } from 'src/functionalUtilities'
+
+import { continueToNextInstruction, loadRegisters } from './helpers'
+
+const setDelayTimer = (delayTimer: number): OpcodeFunc => (chip8State: Chip8): Chip8 => ({
+  ...chip8State,
+  delayTimer
+})
+
+const setSoundTimer = (soundTimer: number): OpcodeFunc => (chip8State: Chip8): Chip8 => ({
+  ...chip8State,
+  soundTimer
+})
+
+/*
+  0xFX07
+  Sets VX to the value of the delay timer.
+*/
+export const setRegisterToDelayTimer = (chip8State: Chip8): Chip8 => {
+  const registerXNumber = chip8Selectors.opcodeRegisterXNumber(chip8State)
+
+  return pipe(
+    loadRegisters({ [registerXNumber]: chip8State.delayTimer }),
+    continueToNextInstruction
+  )(chip8State)
+}
+
+/*
+  0xFX15
+  Sets the delay timer to VX.
+*/
+export const setDelayTimerToRegister = (chip8State: Chip8): Chip8 => {
+  const registerXValue = chip8Selectors.opcodeRegisterXValue(chip8State)
+
+  return pipe(
+    setDelayTimer(registerXValue),
+    continueToNextInstruction
+  )(chip8State)
+}
+
+/*
+  0xFX18
+  Sets the sound timer to VX.
+*/
+export const setSoundTimerToRegister = (chip8State: Chip8): Chip8 => {
+  const registerXValue = chip8Selectors.opcodeRegisterXValue(chip8State)
+
+  return pipe(
+    setSoundTimer(registerXValue),
+    continueToNextInstruction
+  )(chip8State)
+}
