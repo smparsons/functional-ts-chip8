0dfe6edc22026ce9a1c0941b8da233d2ed8ab0d3
Sam Parsons <samuel.m.parsons@gmail.com>
2018-11-11 12:13:50 -0500

    Created selector to retrieve the next opcode from memory.

--- src/chip8/store/selectors.test.ts
+++ src/chip8/store/selectors.test.ts
@@ -3,6 +3,48 @@ import { chip8InitialState } from 'src/chip8/types'
 import { chip8Selectors } from './selectors'
 
 describe('chip8Selectors', () => {
+  describe('getNextOpcodeFromMemory', () => {
+    const currentState = {
+      ...chip8InitialState,
+      memory: Object.assign(Uint8Array.from({ length: 4096 }), chip8InitialState.memory, {
+        [0x240]: 0x8a,
+        [0x241]: 0x74,
+        [0x242]: 0x00,
+        [0x243]: 0xee,
+        [0x244]: 0x05,
+        [0x245]: 0x72,
+        [0x246]: 0xfa,
+        [0x247]: 0x29
+      })
+    }
+
+    it('correctly extracts opcodes', () => {
+      const firstOpcode = chip8Selectors.getNextOpcodeFromMemory({
+        ...currentState,
+        programCounter: 0x240
+      })
+      expect(firstOpcode).toBe(0x8a74)
+
+      const secondOpcode = chip8Selectors.getNextOpcodeFromMemory({
+        ...currentState,
+        programCounter: 0x242
+      })
+      expect(secondOpcode).toBe(0x00ee)
+
+      const thirdOpcode = chip8Selectors.getNextOpcodeFromMemory({
+        ...currentState,
+        programCounter: 0x244
+      })
+      expect(thirdOpcode).toBe(0x0572)
+
+      const fourthOpcode = chip8Selectors.getNextOpcodeFromMemory({
+        ...currentState,
+        programCounter: 0x246
+      })
+      expect(fourthOpcode).toBe(0xfa29)
+    })
+  })
+
   describe('registerXNumber', () => {
     it('parses register x from opcode', () => {
       const currentState = {
--- src/chip8/store/selectors.ts
+++ src/chip8/store/selectors.ts
@@ -4,13 +4,18 @@ const opcodeRegisterXNumber = ({ opcode }: Chip8): number => (opcode & 0x0f00) >
 const opcodeRegisterYNumber = ({ opcode }: Chip8): number => (opcode & 0x00f0) >>> 4
 
 export const chip8Selectors = {
+  getNextOpcodeFromMemory: (chip8State: Chip8): number => {
+    const opcodeFirstHalf = chip8State.memory[chip8State.programCounter]
+    const opcodeSecondHalf = chip8State.memory[chip8State.programCounter + 1]
+    return (opcodeFirstHalf << 8) | opcodeSecondHalf
+  },
   opcodeRegisterXNumber,
   opcodeRegisterYNumber,
-  opcodeRegisterXValue: (chip8State: Chip8) =>
+  opcodeRegisterXValue: (chip8State: Chip8): number =>
     chip8State.vRegisters[opcodeRegisterXNumber(chip8State)],
-  opcodeRegisterYValue: (chip8State: Chip8) =>
+  opcodeRegisterYValue: (chip8State: Chip8): number =>
     chip8State.vRegisters[opcodeRegisterYNumber(chip8State)],
-  opcodeOneDigitConstant: ({ opcode }: Chip8) => opcode & 0x000f,
-  opcodeTwoDigitConstant: ({ opcode }: Chip8) => opcode & 0x00ff,
-  opcodeThreeDigitConstant: ({ opcode }: Chip8) => opcode & 0x0fff
+  opcodeOneDigitConstant: ({ opcode }: Chip8): number => opcode & 0x000f,
+  opcodeTwoDigitConstant: ({ opcode }: Chip8): number => opcode & 0x00ff,
+  opcodeThreeDigitConstant: ({ opcode }: Chip8): number => opcode & 0x0fff
 }
