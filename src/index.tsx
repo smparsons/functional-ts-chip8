3c044d3102a05c9fa88602886968e6dfb70bd8a6
Sam Parsons <samuel.m.parsons@gmail.com>
2018-11-14 19:14:58 -0500

    Connecting to redux store. Moving roms to public folder.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { chip8Reducer } from 'src/chip8/store'
+
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+const store = createStore(chip8Reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+)
+
 registerServiceWorker()
